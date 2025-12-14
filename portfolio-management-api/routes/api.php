98abb1b8164c46ddfd8490f27438f104a517ac2b
Christiann <queenymacaranas62@gmail.com>
2025-12-14 20:45:54 +0800

    fixing skills and experiences for vieing mode

--- portfolio-management-api/routes/api.php
+++ portfolio-management-api/routes/api.php
@@ -14,15 +14,17 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('json.accepts')->get('/public/portfolio', [PublicPortfolioController::class, 'index']);
-
 Route::middleware(['auth:sanctum', 'json.accepts'])->group(function (): void {
-    Route::post('/profile/update', [ProfileController::class, 'update']);
+    //Route::post('/profile/update', [ProfileController::class, 'update']);
 
     Route::get('/projects', [ProjectController::class, 'index']);
     Route::post('/projects/create', [ProjectController::class, 'store']);
     Route::get('/projects/{project}', [ProjectController::class, 'show']);
     Route::put('/projects/update/{project}', [ProjectController::class, 'update']);
     Route::delete('/projects/delete/{project}', [ProjectController::class, 'destroy']);
-    Route::apiResource('skills', SkillController::class)->except(['create', 'edit', 'show']);
-    Route::apiResource('experiences', ExperienceController::class)->except(['create', 'edit', 'show']);
+
+    Route::get('/experiences', [ExperienceController::class, 'index']);
+    Route::get('/skills', [SkillController::class, 'index']);
+   // Route::apiResource('skills', SkillController::class)->except(['create', 'edit', 'show']);
+   // Route::apiResource('experiences', ExperienceController::class)->except(['create', 'edit', 'show']);
 });
