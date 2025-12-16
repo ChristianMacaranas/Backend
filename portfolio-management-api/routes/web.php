0aad3f79460c9e7060a136c06b937686325598ed
Christiann <queenymacaranas62@gmail.com>
2025-12-16 17:29:19 +0800

    new changes

--- portfolio-management-api/routes/web.php
+++ portfolio-management-api/routes/web.php
@@ -4,7 +4,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function() {
-    return response()->json(['message' => 'Backend connected!']);
-});
+            
